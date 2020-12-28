79a0df9fd03c8529f5bb079d0b97c86b96f4e7dc
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2020-12-28 14:53:46 +0100

    changes made in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./common/navigation/NavBar";
 import HomePage from "./features/home/HomePage";
+import Posts from "./features/posts/Posts";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Footer from "./common/footer/Footer";
@@ -8,10 +9,12 @@ import Footer from "./common/footer/Footer";
 function App() {
   return (
     <div className="app">
-      <NavBar/>
+      
       <Router>
+      <NavBar/>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/posts" component={Posts} />
         </Switch>
       </Router>
       <Footer/>
