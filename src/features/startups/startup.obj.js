a2511adbac09512ddba4d18ea755a9207da3d43b
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2020-12-31 22:24:33 +0100

    startup.obj.js created in startup section

---
+++ src/features/startups/startup.obj.js
@@ -0,0 +1,14 @@
+import adobeImage from "../../assets/images/adobe.png";
+import dropBox from "../../assets/images/dropbox.png";
+import airbnb from "../../assets/images/airbnb.png";
+import mercedes from "../../assets/images/mercedes.png";
+
+export const startupsObj = [ { startupTitle:'Adobe', startupImage: adobeImage, location: 'Stockholm, SE', businessType: 'Tech', description: 'Adobe Inc. is an American multinational computer software company. Incorporated in Delaware and headquartered in San Jose...' }, 
+    { startupTitle:'Mercedes', startupImage: mercedes, location: 'Copenhagen, Dk', businessType: 'Tech', description: 'Adobe Inc. is an American multinational computer software company. Incorporated in Delaware and headquartered in San Jose...' }, 
+    { startupTitle:'Drop Box', startupImage: dropBox, location: 'Stockholm, SE', businessType: 'Tech', description: 'Adobe Inc. is an American multinational computer software company. Incorporated in Delaware and headquartered in San Jose...' }, 
+    { startupTitle:'Adobe', startupImage: mercedes, location: 'Copenhagen, Dk', businessType: 'Tech', description: 'Adobe Inc. is an American multinational computer software company. Incorporated in Delaware and headquartered in San Jose...' }, 
+    { startupTitle:'Airbnb', startupImage: airbnb, location: 'Stockholm, SE', businessType: 'Tech', description: 'Adobe Inc. is an American multinational computer software company. Incorporated in Delaware and headquartered in San Jose...' }, 
+    { startupTitle:'Drop Box', startupImage: dropBox, location: 'Copenhagen, Dk', businessType: 'Tech', description: 'Adobe Inc. is an American multinational computer software company. Incorporated in Delaware and headquartered in San Jose...' }, 
+    { startupTitle:'Adobe', startupImage: adobeImage, location: 'Stockholm, SE', businessType: 'Tech', description: 'Adobe Inc. is an American multinational computer software company. Incorporated in Delaware and headquartered in San Jose...' }, 
+    { startupTitle:'Mercedes', startupImage: mercedes, location: 'Copenhagen, Dk', businessType: 'Tech', description: 'Adobe Inc. is an American multinational computer software company. Incorporated in Delaware and headquartered in San Jose...' }
+]
