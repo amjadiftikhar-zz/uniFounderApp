3ff02577d9b05939ab494559b0fb3ffcfa2b2046
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-04 20:15:07 +0100

    posts.obj created

---
+++ src/features/posts/posts.obj.js
@@ -0,0 +1,14 @@
+import adobeImage from "../../assets/images/adobe.png";
+import dropBox from "../../assets/images/dropbox.png";
+import airbnb from "../../assets/images/airbnb.png";
+import mercedes from "../../assets/images/mercedes.png";
+
+export const postsObj = [ { startupTitle:'Adobe', profileTitle:'Manager', startupImage: adobeImage, location: 'Stockholm, SE', businessType: 'Tech', timeStamp: '3 days ago', jobCategory: 'Full time', userType: 'partner' }, 
+    { startupTitle:'Mercedes', profileTitle:'Head, third party strategy deal', startupImage: mercedes, location: 'Copenhagen, Dk', businessType: 'Tech', timeStamp: '3 days ago' , jobCategory: 'Full time', userType: 'partner' }, 
+    { startupTitle:'Drop Box', profileTitle:'Sale Engineer', startupImage: dropBox, location: 'Stockholm, SE', businessType: 'Tech', timeStamp: '3 days ago', jobCategory: 'Full time', userType: 'partner' }, 
+    { startupTitle:'Adobe', profileTitle:'Brand Marketing Manager', startupImage: mercedes, location: 'Copenhagen, Dk', businessType: 'Tech', timeStamp: '3 days ago', jobCategory: 'Full time', userType: 'partner' }, 
+    { startupTitle:'Airbnb', profileTitle: 'Software Developer', startupImage: airbnb, location: 'Stockholm, SE', businessType: 'Tech', timeStamp: '3 days ago', jobCategory: 'Full time', userType: 'partner' }, 
+    { startupTitle:'Drop Box', profileTitle:'Manager', startupImage: dropBox, location: 'Copenhagen, Dk', businessType: 'Tech', timeStamp: '3 days ago', jobCategory: 'Full time', userType: 'partner' }, 
+    { startupTitle:'Adobe', profileTitle:'Marketing Manager', startupImage: adobeImage, location: 'Stockholm, SE', businessType: 'Tech', timeStamp: '3 days ago', jobCategory: 'Full time', userType: 'partner' }, 
+    { startupTitle:'Mercedes', profileTitle:'Designer', startupImage: mercedes, location: 'Copenhagen, Dk', businessType: 'Tech', timeStamp: '3 days ago', jobCategory: 'Full time', userType: 'partner' }
+]
