dbfbcb39e08ad3fa4b9dac817df6aab513402181
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-05 13:39:28 +0100

    team member object added

---
+++ src/features/posts/specificPost/team.obj.js
@@ -0,0 +1,7 @@
+import john from "../../../assets/images/john.png";
+import lara from "../../../assets/images/lara.png";
+
+export const memberObj = [ { memberTitle:'John Doe', jobTitle:'Manager', memberProfileImage: lara}, 
+    { memberTitle:'Lara Smith', jobTitle:'CEO', memberProfileImage: john }, 
+    { memberTitle:'Drop Box', jobTitle:'Co-founder', memberProfileImage: lara} 
+    ]
