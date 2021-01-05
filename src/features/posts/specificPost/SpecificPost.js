cad9a252ecab6b4a5603639d860d425c4b130029
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-05 15:37:06 +0100

    changes made in SpecificPost component

--- src/features/posts/specificPost/SpecificPost.js
+++ src/features/posts/specificPost/SpecificPost.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-import {Grid, Typography} from "@material-ui/core";
+import {Grid, Typography, Button} from "@material-ui/core";
+import location from "../../../assets/images/location.png";
+import business from "../../../assets/images/business.png";
 import Posts from '../Posts';
 import SuggestedPostCard from "./SuggestedPostCard"
 import {postsObj} from "../posts.obj";
@@ -32,6 +34,28 @@ export default function SpecificPost() {
                         />
                     )
                 })}
+                <Grid className={classes.borderSection}>
+                </Grid>
+                <Grid className={classes.suggestedQuerySection}>
+                    <Grid className={classes.suggestedQueryTitle}>
+                        Searching for startup posts?
+                    </Grid>
+                    <Grid className={classes.suggestedPostContainer}>
+                        <Typography className={classes.suggestedInfoSection}>
+                            <Typography className={classes.suggestedQueryInfo}>
+                                <img className={classes.typeImage} src={business} alt=""/> 
+                                What industry are you seeking?
+                            </Typography>
+                            <Typography className={classes.suggestedQueryInfo}>
+                                <img className={classes.typeImage} src={location} alt=""/> Location?
+                            </Typography>
+                        </Typography>
+                        <Button className={classes.searchBtn} color='primary' 
+                            variant="contained">
+                            Search
+                        </Button>
+                    </Grid>
+                </Grid>
                 <a href="">backToAllPosts</a>
             </Grid>
         </Grid>
