3cc003c16d32c8d3af595064a8c053af38d4393f
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-07 18:06:11 +0100

    DialogComponent updated

--- src/features/posts/dialog/DialogComponent.js
+++ src/features/posts/dialog/DialogComponent.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import {Dialog, Grid, Typography, IconButton} from "@material-ui/core";
+import {Dialog, Grid, Typography, Button, 
+    FormControlLabel,  Checkbox
+} 
+from "@material-ui/core";
+import {
+    Formik,
+    Form,
+    Field
+} from 'formik';
+import { TextField } from 'formik-material-ui';
 import { FaFacebookF } from 'react-icons/fa';
 import { FaLinkedinIn } from 'react-icons/fa';
 import { FaGoogle } from 'react-icons/fa';
-
-
-// import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import FacebookIcon from "@material-ui/icons/Facebook";
-// import GmailIcon from "@material-ui/icons/Gmail";
-
-import linkedin from "../../../assets/images/linkedin1.png";
-import facebook from "../../../assets/images/facebook.png";
 import {useStyles, StyledButton} from "./styles.dialog";
 
 function DialogComponent(props) {
     const classes = useStyles();
-    const {onClose,anyValue, open} = props;
+    const {onClose, value, openDialog} = props;
 
-    const handleClose = () => {
-        onClose(anyValue)
-    }
+    const handleCloseDialog = () => {
+        onClose(value)
+    }    
 
     return (
         <Dialog className={classes.dialogContainer} 
-            onClose={handleClose} open={open}>
+            onClose={handleCloseDialog} open={openDialog}>
             <Grid className={classes.dialogHeaderSection}>
                 <Grid className={classes.textSection}>
                     <Typography className= {classes.dialogHeader}>
@@ -49,11 +50,61 @@ function DialogComponent(props) {
                             Facebook
                         </StyledButton>
                     </Typography>
-                   
+                    <Formik
+                        initialValues={{
+                            email: '',
+                            password: '',
+                        }}
+                    >
+                        {({ submitForm, isSubmitting }) => (
+                        <Form>
+                            <Field
+                                type="text"
+                                component={TextField}
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                                fullWidth
+                                margin="normal"
+                                variant="outlined"
+                                className={classes.textFieldStyle}
+                            />
+                            <br />
+                            <Field
+                                type="password"
+                                component={TextField}
+                                id="password"
+                                name="password"
+                                placeholder="Password"
+                                fullWidth
+                                margin="normal"
+                                variant="outlined" 
+                                className={classes.textFieldStyle}
+                            />
+                            <FormControlLabel
+                                control={
+                                <Checkbox
+                                    // checked={state.checkedB}
+                                    // onChange={handleChange}
+                                    name="checkedB"
+                                    color="primary"
+                                />
+                                }
+                                label="Remember me?"
+                            />
+                            <Button
+                                className={classes.submitBtn}
+                                variant="contained"
+                                color="primary"
+                                // disabled={isSubmitting}
+                                // onClick={submitForm}
+                            >
+                                Log In
+                            </Button>
+                        </Form>
+                        )}
+                    </Formik>
                 </Grid> 
-            </Grid>
-            <Grid className={classes.dialogHeaderSection}>
-                Form here
             </Grid>
         </Dialog>
     )
