f1207492cfb5518d6beb119da0b2a9ba6a120a09
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-07 18:07:09 +0100

    styles.dialog updated

--- src/features/posts/dialog/styles.dialog.js
+++ src/features/posts/dialog/styles.dialog.js
@@ -1,8 +1,8 @@
 import {makeStyles, withStyles} from "@material-ui/core";
-// import { withStyles } from '@material-ui/core/styles';
 import { Button, IconButton } from '@material-ui/core';
 
 const StyledButton = withStyles({
+    // Customizing buttons
     root: {
         background: '#0073B0',
         border: 0,
@@ -22,29 +22,67 @@ const StyledButton = withStyles({
 })(Button);
 
 const StyledIconButton = withStyles({
+    // Customizing buttons
     root: {
-        // background: 'white',
-        // border: 0,
-        // height:"40px",
-        color: '#0073B0',
-        // padding: '0 25px',
-        // fontSize: 16,
-        // fontWeight: 500,
-        // textTransform: "none",
-        // margin: "5px 0",
-        
+        color: '#0073B0',        
     },
 
 })(IconButton);
 
 const useStyles = makeStyles(() => ({
+    // Confirmation dialog box styles
+    confirmationHeaderSection: {
+        marginTop: '5em',
+        padding: '2em',
+    },
+    confirmationButtons: {
+        marginTop: '3em',
+    },
+    declineBtn: {
+        width:'125px', 
+        background:"#F1F4FD", 
+        color:'blue', 
+        marginRight:'0.8em', 
+        textTransform:'none',
+        fontFamily: 'poppins',
+        fontWeight: 400,
+        fontSize: 16,
+        fontStyle: 'normal',
+    }, 
+    applyBtn: {
+        width:'125px', 
+        background:"#3936C4",
+        textTransform: 'none',
+        color:'white',
+        fontFamily: 'poppins',
+        fontWeight: 400,
+        fontSize: 16,
+        fontStyle: 'normal',
+        '&:hover': {
+            background: '#0073B0',
+        },
+    },
+    //temporary button for 'not signed in' styles
+    notSignedinBtn: {
+        width: 100,
+        background:"#3936C4",
+        textTransform: 'none',
+        color:'white',
+        fontFamily: 'poppins',
+        fontWeight: 400,
+        fontSize: 14,
+        marginLeft: '0.8em',
+        fontStyle: 'normal',
+        '&:hover': {
+            background: '#0073B0',
+        },
+    },
+    // dialog box styles
     dialogContainer: {
-        // padding: '5em',
-        // width: '20em',
-        // height: '20em',
+        borderRadius: '20px',
+        border: '2px solid black',
     },
     dialogHeaderSection: {
-        // border: '2px solid red',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
@@ -61,7 +99,6 @@ const useStyles = makeStyles(() => ({
         color: '#0E0E31',
     },
     signupContent: {
-        // border: '2px solid black',
         fontFamily: 'Poppins',
         fontSize: '14px',
         fontStyle: 'normal',
@@ -71,8 +108,8 @@ const useStyles = makeStyles(() => ({
         textAlign: 'left',
         color: '#0E0E31',
     },
+    //buttons and button section style
     buttonSection: {
-        // border: '2px solid yellow',
         margin: '1em',
         width: '22em',
         display: 'flex',
@@ -82,6 +119,7 @@ const useStyles = makeStyles(() => ({
         display: 'flex',
         justifyContent: 'space-between',
     },
+    // Social media Icons styles
     iconClass: {
         width: 16,
         height: 16,
@@ -89,12 +127,26 @@ const useStyles = makeStyles(() => ({
         alignSelf: 'center',
         marginRight: '0.8em',
     },
-    // socialButtons: {
-    //     display: 'flex',
-    //     border: '2px solid black',
-    //     alignItems: 'center',
-
-    // }
+    textFieldStyle: {
+        width: '100%',
+        height: 30,
+        borderRadius: '3px',
+        marginBottom: '1.3em',
+    },
+    // submit button style
+    submitBtn: {
+        marginTop: '3em',
+        width: '100%',
+        borderRadius: '5px',
+        fontFamily: 'poppins',
+        textTransform: 'none',
+        fontSize: '14px',
+        fontStyle: 'normal',
+        fontWeight: 500,
+        lineHeight: '21px',
+        letterSpacing: '0px',
+        textAlign: 'center',
+    },
 }));
 
 export {useStyles, StyledButton, StyledIconButton}
