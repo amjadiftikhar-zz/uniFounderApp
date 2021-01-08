3ebd80d4f02f95e4dbfb6bf3be0d105497e35c93
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-08 21:16:11 +0100

    checkbox style fixed in postCategory

--- src/features/posts/PostCategory.js
+++ src/features/posts/PostCategory.js
@@ -11,102 +11,102 @@ function PostCategory() {
             <Grid className={classes.typeOfBusiness}>
                 <Typography className={classes.categoryTitle} variant="h4">Field</Typography>
                 <Typography className={classes.element}> 
-                    <CustomCheckbox className={classes.checkbox} /> 
+                    <CustomCheckbox color='primary' className={classes.checkbox} /> 
                     Marketing & Communication
                 </Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Business Development</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Full-Stack Developmnt</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Backend Development</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Frontend Development</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Project Management</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Business Development</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Customer Service</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Sales</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Analysis</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Data Scientist</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Graphic Design</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     UX/UI Design</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Finance</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Engineering</Typography> 
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     <a>See less</a></Typography>
             </Grid> 
             <Grid className={classes.typeOfBusiness}>
                 <Typography className={classes.categoryTitle} variant="h4">
                     Industory</Typography>
                 <Typography className={classes.element}> 
-                    <CustomCheckbox className={classes.checkbox} /> 
+                    <CustomCheckbox color='primary' className={classes.checkbox} /> 
                     Technology
                 </Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Transport</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Agriculture</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Education</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Healthcare</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Food</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     News & Media</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Arts & Design</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     <a>See less</a></Typography> 
             </Grid>
             <Grid className={classes.typeOfBusiness}>
                 <Typography className={classes.categoryTitle} variant="h4">
                     Compensation</Typography>
                 <Typography className={classes.element}> 
-                    <CustomCheckbox className={classes.checkbox} /> 
+                    <CustomCheckbox color='primary' className={classes.checkbox} /> 
                     Equity
                 </Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Salary</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Wage</Typography>
                 <Typography className={classes.element}>
-                    <CustomCheckbox className={classes.checkbox} />
+                    <CustomCheckbox color='primary' className={classes.checkbox} />
                     Commission</Typography>                                    
             </Grid>
         </Grid>
