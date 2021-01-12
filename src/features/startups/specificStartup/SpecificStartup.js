89292d21d29c86c3cacbde0766606b27248c6003
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-12 23:55:37 +0100

    SpecificStartup.js updated

--- src/features/startups/specificStartup/SpecificStartup.js
+++ src/features/startups/specificStartup/SpecificStartup.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Grid, Typography, Button} from "@material-ui/core";
+import ChevronRight from "@material-ui/icons/ChevronRight";
 import location from "../../../assets/images/location.png";
 import business from "../../../assets/images/business.png";
 // import Posts from '../Posts';
@@ -18,9 +19,9 @@ export default function SpecificStartup() {
         <>
         {/* {toggleToPosts ? <Posts/> :  */}
         <Grid className={classes.specificStartupPostContainer}>
-            <Grid className={classes.specificDetailSection}>
+            {/* <Grid className={classes.specificDetailSection}> */}
                 <SpecificStartupDetails/>
-            </Grid>
+            {/* </Grid> */}
             <Grid className={classes.relevantStartupSection}>
                 <h4>Suggested Startups</h4>
                 <Grid className={classes.suggestedStartupCard}>
@@ -46,13 +47,13 @@ export default function SpecificStartup() {
                 </Grid>
                 <Grid className={classes.suggestedQuerySection}>
                     <Grid className={classes.suggestedQueryTitle}>
-                        Searching for startup posts?
+                        Looking for startups to join?
                     </Grid>
                     <Grid className={classes.suggestedPostContainer}>
                         <Typography className={classes.suggestedInfoSection}>
                             <Typography className={classes.suggestedQueryInfo}>
                                 <img className={classes.typeImage} src={business} alt=""/> 
-                                What industry are you seeking?
+                                What industry are you seeking??
                             </Typography>
                             <Typography className={classes.suggestedQueryInfo}>
                                 <img className={classes.typeImage} src={location} alt=""/> Location?
@@ -64,7 +65,12 @@ export default function SpecificStartup() {
                         </Button>
                     </Grid>
                 </Grid>
-                <a href="">backToAllStartups</a>
+                <Grid className={classes.expandAll}>
+                <a href="" >
+                    See all Startups
+                </a>
+                    <ChevronRight className={classes.chevron}/> 
+                </Grid>
             </Grid>
         </Grid>
         {/* } */}
