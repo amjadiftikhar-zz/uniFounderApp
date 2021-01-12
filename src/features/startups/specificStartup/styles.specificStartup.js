90b840c3aaace3a6d53900c343d024e19a391388
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-12 23:56:30 +0100

    styles edited and added

--- src/features/startups/specificStartup/styles.specificStartup.js
+++ src/features/startups/specificStartup/styles.specificStartup.js
@@ -5,33 +5,34 @@ export const useStyles = makeStyles(() => ({
   specificStartupPostContainer: {
     border: '2px solid red',
     display: 'flex',
-    justifyContent: 'space-between',
-    background: '#FFFFFF',
-    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    flexWrap: 'wrap',
+    margin: '3em 0 0 2em',
   },
-  specificStartupMainSection: {
-    border: '2px solid skyblue',
+  specificStartupCardSection: {
+    border: '2px solid yellow',
+    width: '73%',
     background: '#FFFFFF',
     boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'space-between',
+    background: '#FFFFFF',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+  },
+  startupBannerSection: {
+    width: '100%',
   },
   startupBanner: {
+    border: '2px solid black',
+    width: '100%',
     borderTopRightRadius: 10,
     borderTopLeftRadius: 10,
   },
   //styles specific starup left section card with details about startup 
   startupCardSection: {
     border: '2px solid black',
-    width: '100%',
     display: 'flex',
-    // justifyContent: 'space-between',
     alignItems: 'center',
-    // marginRight: '1em',
-    // width: '100%',
-    // background: '#FFFFFF',
-    // padding: '1em',
+    padding: '1em',
   }, 
   imageSection: {
     width: 150,
@@ -42,7 +43,7 @@ export const useStyles = makeStyles(() => ({
     height: '100%'
   },
   startupTitleSection: {
-    // border: '2px solid red',
+    border: '2px solid red',
     display: 'flex',
     flexDirection: 'column',
     marginRight: '1em',
@@ -50,7 +51,6 @@ export const useStyles = makeStyles(() => ({
     width: '100%'
   },
   profileTitle: {
-    // border: '2px solid white',
     textAlign: 'left',
     fontFamily: 'Poppins',
     fontSize: '35px',
@@ -70,13 +70,12 @@ export const useStyles = makeStyles(() => ({
     background: '#F8FAFF',
     borderRadius: 6,
     marginTop: '1.5em',
-    width: '35em',
+    width: '65%',
     padding: '2em 2em 0 2em',
     display: 'flex',
     flexDirection: 'column',
   },
   detailType: {
-    // border: '2px solid red',
     fontFamily: 'Poppins',
     fontSize: '14px',
     fontStyle: 'normal',
@@ -86,21 +85,21 @@ export const useStyles = makeStyles(() => ({
     alignItems: 'center',
   },
   typeImage: {
-    // border: '2px solid black',
     marginRight: '0.5em',
     width: 15,
     height: 15
   },
   detailTypeSection: {
-    // border: '2px solid black',
     marginTop: '0.5em',
     display: 'flex',
+    flexWrap: 'wrap',
     justifyContent: 'space-between',
   },
   //styles specific starup description section
   specificStartupDescription:{
     border: '2px solid red',
     textAlign: 'left',
+    padding: '0 3em 0 3em',
   },
   sectionTitle: {
     fontFamily: 'Poppins',
@@ -130,12 +129,12 @@ export const useStyles = makeStyles(() => ({
   },
 //styles specific starup right section suggested startups cards 
   relevantStartupSection: {
-    border: '2px solid blue',
-    width: '30em',
+    // border: '2px solid blue',
+    // width: '25%',
   },
   suggestedStartupCard: {
-    marginRight: '1em',
-    marginLeft: '1em',
+    width: '25%',
+    marginLeft: '2em',
   },
  savedSection: {
     display: 'flex',
@@ -150,6 +149,79 @@ export const useStyles = makeStyles(() => ({
   contactBtn: {
     marginTop: '2em',
     textTransform: 'none',
-  }
+  },
+  suggestedQueryTitle: {
+    border: '2px solid red',
+    width: '10.5em',
+    textAlign: 'left',
+    background: '#F1F4FD',
+    height: '3em',
+    padding: '1em',
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: '20px',
+    lineHeight: '30px',
+    color: '#242F65',
+  },
+  // styles, border between two sections
+  borderSection: {
+    width: '25%',
+    width: '251px',
+    height: '0px',
+    left: '1123px',
+    top: '1299px',
+    border: '1px solid #D8D8D8',
+    marginTop: '2em',
+    marginLeft: '2em',
+    marginBottom: '2em',
+  },
+  suggestedInfoSection: {
+    paddingTop: '1em',
+    paddingBottom: '1em'
+  },
+  suggestedPostContainer: {
+    width: '15em',
+    boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.1)',
+    background: '#FFFFFF',
+    borderRadius: 10,
+    padding: '0.5em',
+    marginBottom: '1em',
+  },
+  suggestedQueryInfo: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '12px',
+    lineHeight: '15px',
+    color: '#6E727E',
+    marginBottom: '1em',
+  },
+  suggestedQuerySection: {
+    width: '15em',
+    textAlign: 'left',
+    marginLeft: '2em',
+  },
+  searchBtn: {
+    width: '100%',
+  },
+  expandAll: {
+    width: '14em',
+    marginLeft: '2em',
+    padding: '1em',
+    border: '2px solid red',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '& a': {
+        fontFamily: 'Poppins',
+        fontSize: '16px',
+        fontStyle: 'normal',
+        fontWeight: 500,
+        lineHeight: '24px',
+        letterSpacing: '0px',
+        textAlign: 'center',  
+    }    
+  },
 }));
 
